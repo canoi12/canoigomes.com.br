f156e9074fa08945724fb34790e845ab05f93798
Canoi Gomes <canoi.aguiar@megacatstudios.com>
2024-07-23 22:33:09 -0300

    updated server.py to show projects pages;

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ os.chdir(build_dir)
 
 class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self) -> None:
-        if self.path in ['/', '/index.html', '/about', '/archive', '/projects'] or self.path.startswith('/post/') or self.path.startswith('/tag/') or self.path.startswith('/search'):
+        if self.path in ['/', '/index.html', '/about', '/archive'] or self.path.startswith('/post/') or self.path.startswith('/tag/') or self.path.startswith('/search') or self.path.startswith('/project'):
             self.path = 'index.html'
         # self.path = 'index.html'
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
