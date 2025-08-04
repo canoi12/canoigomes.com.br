44c64beb0f241b1806ff49c596b91d508aa54cad
Canoi <canoiaguiar@gmail.com>
2025-08-04 00:34:22 -0300

    updated scripts;

--- scripts/build.js
+++ scripts/build.js
@@ -1,23 +1,58 @@
-$("#site-header").load("/partials/header.html", function(response, status, xhr) {
-    if (status === "success") {
-        console.log("External HTML loaded successfully!");
-        const buttons = ['home', 'resume', 'projects', 'decentralize'];
-            for (let i in buttons) {
-                // console.log(buttons[i])
-                let name = buttons[i]
-                const btn = document.getElementById(name + '_button');
-                // console.log(btn);
-                if (btn) btn.innerHTML = sidebar_texts[lang][name]
+function loadHeader() {
+    return new Promise ((res, rej) => {
+        $("#site-header").load("/partials/header.html", function(response, status, xhr) {
+            if (status === "success") {
+                res();
+            } else {
+                rej("Failed to load: " + xhr.status + " " + xhr.statusText);
             }
-        // Further actions after the content is in the DOM
-    } else if (status === "error") {
-        console.error("Error loading external HTML: " + xhr.status + " " + xhr.statusText);
+        });
+    });
+}
+
+var navShow = false;
+let navDiv = document.getElementById('nav-menu');
+
+loadHeader().then(() => {
+    console.log("External HTML loaded successfully!");
+    if (!navDiv) navDiv = document.getElementById('nav-menu');
+    const buttons = ['home', 'resume', 'projects', 'decentralize'];
+    for (let i in buttons) {
+        // console.log(buttons[i])
+        let name = buttons[i]
+        const btn = document.getElementById(name + '_button');
+        // console.log(btn);
+        if (btn) btn.innerHTML = sidebar_texts[lang][name]
     }
-});
+}).catch(err => {
+    console.error(err);
+})
+
+// $("#site-header").load("/partials/header.html", function(response, status, xhr) {
+//     if (status === "success") {
+//         console.log("External HTML loaded successfully!");
+//         console.log($('#site-header').getElementById('nav-menu'));
+//         if (!navDiv) navDiv = $('#site-header').getElementById('nav-menu');
+//         console.log(navDiv);
+//         const buttons = ['home', 'resume', 'projects', 'decentralize'];
+//         for (let i in buttons) {
+//             // console.log(buttons[i])
+//             let name = buttons[i]
+//             const btn = document.getElementById(name + '_button');
+//             // console.log(btn);
+//             if (btn) btn.innerHTML = sidebar_texts[lang][name]
+//         }
+//         // Further actions after the content is in the DOM
+//     } else if (status === "error") {
+//         console.error("Error loading external HTML: " + xhr.status + " " + xhr.statusText);
+//     }
+// });
+
 console.log("Header loaded");
 
 function updateNavMenu() {
     if (!navDiv) navDiv = document.getElementById('nav-menu');
+    if (!navDiv) return;
     let x = window.matchMedia('screen and (max-width: 719px)');
     if (!x.matches) {
         navShow = true;
